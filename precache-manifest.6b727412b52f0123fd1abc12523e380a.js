c2784e1208ffdd95e10dff3c047dcef71a81a3b8
Anita Jayana <anje12sth@gmail.com>
2022-04-19 14:58:26 +1000

    Updates

---
+++ precache-manifest.6b727412b52f0123fd1abc12523e380a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0e796fe0f9cc770dbbc10eb36e5bc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53c83efe844dcaa5cb37",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d63e89d434d5c9a8bd21",
+    "url": "./static/js/2.b2f455bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b2f455bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c83efe844dcaa5cb37",
+    "url": "./static/js/main.ecf83156.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
